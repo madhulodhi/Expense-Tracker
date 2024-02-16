752d21849e46a5cb44ba84679266e30dff0800e8
MADHU LODHI <132137517+madhulodhi@users.noreply.github.com>
2024-02-16 08:51:06 +0530

    Add files via upload

---
+++ expense-tracker/src/App.tsx
@@ -0,0 +1,24 @@
+import { Route, Routes } from "react-router";
+import { BrowserRouter } from "react-router-dom";
+import "./App.css";
+import ExpenseTracker from "./components/ExpenseTracker";
+import ShowData from "./components/ShowData";
+import React from "react";
+
+function App() {
+  return (
+    <div className="App">
+      <BrowserRouter>
+        <Routes>
+          <Route
+            path="/add"
+            element={<ExpenseTracker onTrue={undefined} onClose={undefined} />}
+          />
+          <Route path="/" element={<ShowData />} />
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
+}
+
+export default App;
